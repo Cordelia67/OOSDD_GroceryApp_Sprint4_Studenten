d8c690bc5abf62d1e14793a5e561797443623654
s1218918 <s1218918@student.windesheim.nl>
2025-10-03 15:35:37 +0200

    Methode GetBestSellingProducts uitgewerkt. Het pakt de producten uit groceriesRepository en filtert ze op ProductID. Daar wordt berekent hoe vaak ze gekocht zijn. Deze worden in een lijst gezet, de andere statistieken worden opgehaald, en de producten worden in rankorde gezet.

--- Grocery.Core/Services/GroceryListItemsService.cs
+++ Grocery.Core/Services/GroceryListItemsService.cs
@@ -51,7 +51,39 @@ namespace Grocery.Core.Services
 
         public List<BestSellingProducts> GetBestSellingProducts(int topX = 5)
         {
-            throw new NotImplementedException();
+            List<GroceryListItem> allProducts = _groceriesRepository.GetAll();
+
+            // group products by ProductId and calculate the total amount sold
+            var ProductSales = allProducts
+                .GroupBy(item => item.ProductId)  
+                .Select(group => new  // create an object for each group with the Id & amount sold
+                {
+                    ProductId = group.Key,  
+                    TotalSold = group.Sum(product => product.Amount)  
+                })
+                .OrderByDescending(x => x.TotalSold)  // sort highest amount sold down to the lowest
+                .Take(topX)  
+                .ToList();  
+
+            List<BestSellingProducts> result = new List<BestSellingProducts>(); // make the final list to put the proper results into
+            int ranking = 1; 
+            foreach (var amountSold in ProductSales) // loop through the top results and make them into objects with all product info
+            {
+                Product? product = _productRepository.Get(amountSold.ProductId); // Get product detail from productrepository
+                if (product != null)
+                {
+                    BestSellingProducts bestSeller = new BestSellingProducts(
+                        productId: product.Id,
+                        name: product.Name,
+                        stock: product.Stock,
+                        nrOfSells: amountSold.TotalSold,  
+                        ranking: ranking  
+                    );
+                    result.Add(bestSeller);
+                    ranking++; 
+                }
+            }
+            return result;
         }
 
         private void FillService(List<GroceryListItem> groceryListItems)
